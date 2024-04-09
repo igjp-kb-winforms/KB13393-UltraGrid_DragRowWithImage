f260e2d16c78b112b45e72ec7f1a4e3a505e7adf
mkamiishi <mkamiishi@infragistics.com>
2024-04-09 17:33:01 +0900

    Initial commit
    初回コミット

---
+++ UltraGrid_DragRowWithImage/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace UltraGrid_DragRowWithImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ultraGrid1 = new Infragistics.Win.UltraWinGrid.UltraGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraGrid1
+            // 
+            this.ultraGrid1.Location = new System.Drawing.Point(22, 12);
+            this.ultraGrid1.Name = "ultraGrid1";
+            this.ultraGrid1.Size = new System.Drawing.Size(514, 448);
+            this.ultraGrid1.TabIndex = 0;
+            this.ultraGrid1.Text = "ultraGrid1";
+            this.ultraGrid1.SelectionDrag += new System.ComponentModel.CancelEventHandler(this.ultraGrid1_SelectionDrag);
+            this.ultraGrid1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ultraGrid1_DragDrop);
+            this.ultraGrid1.DragOver += new System.Windows.Forms.DragEventHandler(this.ultraGrid1_DragOver);
+            this.ultraGrid1.DragLeave += new System.EventHandler(this.ultraGrid1_DragLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 473);
+            this.Controls.Add(this.ultraGrid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.UltraWinGrid.UltraGrid ultraGrid1;
+    }
+}
+
---
+++ UltraGrid_DragRowWithImage/Form1.cs
@@ -0,0 +1,118 @@
+﻿using Infragistics.Win;
+using Infragistics.Win.UltraWinGrid;
+using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UltraGrid_DragRowWithImage
+{
+    public partial class Form1 : Form
+    {
+        DragDropIndicatorManager ind;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ultraGrid1.DataSource = getTable();
+
+            ultraGrid1.DisplayLayout.Override.SelectTypeRow = SelectType.Extended;
+            ultraGrid1.DisplayLayout.Override.CellClickAction = CellClickAction.RowSelect;
+            ultraGrid1.AllowDrop = true;
+        }
+
+        private DataTable getTable()
+        {
+            DataTable table = new DataTable();
+
+            table.Columns.Add("col1", typeof(Int32));
+            table.Columns.Add("col2", typeof(string));
+            table.Columns.Add("col3", typeof(string));
+            table.Columns.Add("col4", typeof(string));
+            table.Columns.Add("col5", typeof(string));
+
+            for (int i = 0; i < 100; i++)
+                table.Rows.Add(new object[] { i, "test" + i.ToString(), "test", "test", "test" });
+
+            return table;
+        }
+
+        private void ultraGrid1_SelectionDrag(object sender, CancelEventArgs e)
+        {
+            //DragDropIndicatorManagerを作成する
+            ind = new DragDropIndicatorManager(Orientation.Horizontal, null);
+            List<UIElement> elements = new List<UIElement>();
+            foreach (UltraGridRow row in ultraGrid1.Selected.Rows)
+            {
+                elements.Add(row.GetUIElement());
+            }
+            ind.InitializeDragIndicator(elements.ToArray());
+            ind.DragIndicatorOffset = new Point(-50, 0);
+            ind.ShowDragIndicator(System.Windows.Forms.Control.MousePosition);
+
+            //UltraGridのドラッグを開始する
+            ultraGrid1.DoDragDrop(ultraGrid1.Selected.Rows, DragDropEffects.Move);
+        }
+
+        private void ultraGrid1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            UltraGrid grid = sender as UltraGrid;
+            Point pointInGridCoords = grid.PointToClient(new Point(e.X, e.Y));
+            // スクロール位置の調整
+            if (pointInGridCoords.Y < 20)
+                grid.ActiveRowScrollRegion.Scroll(RowScrollAction.LineUp);
+            else if (pointInGridCoords.Y > grid.Height - 20)
+                grid.ActiveRowScrollRegion.Scroll(RowScrollAction.LineDown);
+
+            if (ind != null)
+            {
+                //マウスに合わせてDragDropIndicatorManagerを移動させる
+                ind.ShowDragIndicator(System.Windows.Forms.Control.MousePosition);
+            }
+        }
+
+        private void ultraGrid1_DragDrop(object sender, DragEventArgs e)
+        {
+            int dropIndex;
+            UltraGrid grid = sender as UltraGrid;
+
+            // ドロップされた位置を取得する
+            UIElement uieOver = grid.DisplayLayout.UIElement.ElementFromPoint(grid.PointToClient(new Point(e.X, e.Y)));
+
+            // ドロップ位置の行を取得する
+            UltraGridRow ugrOver = uieOver.GetContext(typeof(UltraGridRow), true) as UltraGridRow;
+            if (ugrOver != null)
+            {
+                dropIndex = ugrOver.Index;
+
+                // 選択行を取得する
+                SelectedRowsCollection SelRows = (SelectedRowsCollection)e.Data.GetData(typeof(SelectedRowsCollection));
+                foreach (UltraGridRow aRow in SelRows)
+                {
+                    //選択された行をドロップ領域に移動します
+                    ultraGrid1.Rows.Move(aRow, dropIndex);
+                }
+            }
+            //DragDropIndicatorManagerを非表示とする
+            ind.HideDragIndicator();
+
+        }
+
+        private void ultraGrid1_DragLeave(object sender, EventArgs e)
+        {
+            if (ind != null)
+            {
+                ind.HideDragIndicator();
+            }
+        }
+    }
+}
